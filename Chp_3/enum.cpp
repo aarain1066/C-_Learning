5913e93cad2aee4201a88352912928c78aafbd89
Asim Arain <aarain1066@gmail.com>
2021-09-28 21:47:41 -0400

    enum example

---
+++ Chp_3/enum.cpp
@@ -0,0 +1,25 @@
+// Practice w/ enumerations
+
+#include <iostream>
+using namespace std;
+
+enum CardinalDirections {
+
+	North = 25,
+	South,
+	East,
+	West
+};
+
+int main() {
+	cout << "Displaying directions and their symbolic values" << endl;
+	cout << "North: " << North << endl;
+	cout << "South: " << South << endl;
+	cout << "East: " << East << endl;
+	cout << "West: " << West << endl;
+
+	CardinalDirections windDirection = South;
+	cout << "Variable winDirection: " << windDirection << endl;
+
+	return 0;
+}
